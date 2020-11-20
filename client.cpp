8f1570e3348f149c2a66ad23a191e8cf55e51581
Rodrigo Amaral <rodrigoaf61@gmail.com>
2020-11-19 23:16:35 -0300

    Improve the gameloop from the client.

--- client.cpp
+++ client.cpp
@@ -10,52 +10,82 @@
 
 using namespace std;
 
+int device_fd;
+char buf[255];
+
+/**
+ * Read the board from the device and print in the console. Return false if the read failed.
+*/
+bool print_board() 
+{
+    ssize_t read_count = read(device_fd, buf, BOARD_SZ);
+    if(read_count >= 0)
+    {
+        printf("-----------\n");
+        printf("Minesweeper\n");
+        printf("-----------\n");
+        for (int i = 0; i < read_count; i++)
+        {
+            if (i != 0 && i % COL_COUNT == 0)
+                printf("\n");
+            printf("%c", buf[i]);
+        }
+        printf("\n");
+    }
+    else
+    {
+        printf("Read failed.\n");
+        return false;
+    }
+        
+    return true;
+}
+
+bool play(int i, int j) 
+{
+    string play = to_string(i * COL_COUNT + j);
+    ssize_t count = write(device_fd, play.c_str(), play.size());
+    if(count < 0)
+    {
+        printf("Write failed.\n");
+        return false;
+    }
+
+    return true;
+}
+
+void open_device()
+{
+    device_fd = open("/dev/minesweeper", O_RDWR);
+    if(device_fd < 0)
+    {
+        printf("No device found! fd = %d\n", device_fd);
+        exit(1);
+    }
+}
+
+void close_device() 
+{
+    if (device_fd >= 0)
+        close(device_fd);
+}
+
 int main() 
 {
-    int choice;
-    while (printf("0 to read, 1 to write, 2 to exit\n"), scanf("%d", &choice), choice != 2)
+    open_device();
+
+    while (true)
     {
-        if (choice == 0) 
-        {
-            int fd = open("/dev/minesweeper", O_RDONLY);
-            if(fd < 0)
-                return !printf("No device found! fd = %d\n", fd);
+        if (!print_board())
+            break;
 
-            char buf[255];
-            ssize_t read_count = read(fd, buf, BOARD_SZ);
-            if(read_count >= 0)
-            {
-                printf("count = %d\n", read_count);
-                for (int i = 0; i < read_count; i++)
-                {
-                    if (i != 0 && i % COL_COUNT == 0)
-                        printf("\n");
-                    printf("%c", buf[i]);
-                }
-                printf("\n");
-            }
-            else
-                printf("Read failed.\n");
-
-            close(fd);   
-        }
-        else
-        {
-            int fd = open("/dev/minesweeper", O_WRONLY);
-            if(fd < 0)
-                return !printf("No device found! fd = %d\n", fd);
-
-            int i, j;
-            printf("type coord i, j: ");
-            scanf("%d%d", &i, &j);
-            string play = to_string(i * COL_COUNT + j);
-            ssize_t count = write(fd, play.c_str(), play.size());
-            if(count >= 0)
-                printf("count = %d\n", count);
-            else
-                printf("Write failed.\n");
-
-            close(fd);   
-        }
+        int i, j;
+        printf("Type the coord i, j (no comma): ");
+        scanf("%d%d", &i, &j);
+        
+        if (!play(i, j))
+            break;
     }
+
+    close_device();
 }
